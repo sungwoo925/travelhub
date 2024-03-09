ede64f113b48f9819d045b4f8797a5f62215c9b4
sungwoo925 <qkrtjddn5807@gmail.com>
2024-03-10 02:09:33 +0900

    Studio fun Test

--- travelhub_front/src/App.js
+++ travelhub_front/src/App.js
@@ -4,11 +4,11 @@ import Home from './pages/Home/Home';
 import About from './pages/About/About';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
-import './App.css'; // 전역 스타일 파일
 import Login from './pages/Login/Login';
 import Register from './pages/Register/Register';
 import Record from './pages/Record/Record';
 import Studio from './studio/Studio';
+import './App.css';
 
 function App() {
   console.log("hi");
@@ -16,11 +16,11 @@ function App() {
     <Router>
       <div className="app">
         <Routes>
-          <Route path="/" element={<div><Header /><Home /><Footer /></div>} />
-          <Route path="/about" element={<div><Header /><About /><Footer /></div>} />
-          <Route path="/record" element={<div><Header /><Record /><Footer /></div>} />
-          <Route path="/login" element={<div><Header /><Login /><Footer /></div>} />
-          <Route path="/register" element={<div><Header /><Register /><Footer /></div>} />
+          <Route path="/" element={<div><Header /><div className='empty'/><Home /><Footer /></div>} />
+          <Route path="/about" element={<div><Header /><div className='empty'/><About /><Footer /></div>} />
+          <Route path="/record" element={<div><Header /><div className='empty'/><Record /><Footer /></div>} />
+          <Route path="/login" element={<div><Header /><div className='empty'/><Login /><Footer /></div>} />
+          <Route path="/register" element={<div><Header /><div className='empty'/><Register /><Footer /></div>} />
           <Route path="/studio" element={<Studio/>}/>
         </Routes>
       </div>
