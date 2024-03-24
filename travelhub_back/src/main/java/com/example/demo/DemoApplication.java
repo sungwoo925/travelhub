952766edb75301d74b54c1bc87d908bda041ccec
sungwoo925 <qkrtjddn5807@gmail.com>
2024-03-25 00:15:57 +0900

    JSON Add

--- travelhub_back/src/main/java/com/example/demo/DemoApplication.java
+++ travelhub_back/src/main/java/com/example/demo/DemoApplication.java
@@ -1,5 +1,8 @@
 package com.example.demo;
 
+import org.json.JSONObject;
+import java.io.FileReader;
+import java.io.IOException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -23,6 +26,31 @@ public class DemoApplication {
     @CrossOrigin(origins = "http://localhost:8888")
     @GetMapping("/json")
     public String getJson() {
+        getJsonA("./secinfo.json","databaseIp");
         return "{\"message\": \"hello\"}";
+    }
+
+    public static String getJsonA(String filePath, String name){
+        // filePath = "./secinfo.json";
+        // name = "databasIp";
+
+        try {
+
+            FileReader reader = new FileReader(filePath);
+
+            StringBuilder content = new StringBuilder();
+            int character;
+            while ((character = reader.read()) != -1) {
+                content.append((char) character);
+            }
+
+            JSONObject json = new JSONObject(content.toString());
+            System.out.println(json.getString(name));  
+            reader.close();
+            return json.getString(name);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "error";
+        }
     }
 }
