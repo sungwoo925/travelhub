80a3c2bce69583b0474260a3376a4b25d9bb8c38
sungwoo925 <qkrtjddn5807@gmail.com>
2024-04-03 22:51:02 +0900

    studio dev

--- travelhub_front/src/studio/Studio.js
+++ travelhub_front/src/studio/Studio.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import * as THREE from 'three';
 import './Studio.css';
 
@@ -25,16 +25,24 @@ function addImagePlane(scene, imagePath, position, width, objectName, rotation) 
   });
 }
 
-
-
 const Studio = () => {
   const containerRef = useRef();
   const cameraRef = useRef();
+  const [mapJson,SetMapJson] = useState(null);
+
+  if(!mapJson){
+    fetch(process.env.PUBLIC_URL + './mapinfo.json')
+      .then(response => response.json())
+      .then( mapJson => SetMapJson(mapJson) )
+      .catch(error => console.log('error'))
+  }
 
   useEffect(() => {
     // Scene
     const scene = new THREE.Scene();
-
+    const textureLoader = new THREE.TextureLoader();
+    const texture = textureLoader.load('./images/20230824_134034.jpg');
+    scene.background = texture;
     // Camera
     const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
     camera.position.y = 2;
@@ -45,21 +53,18 @@ const Studio = () => {
     renderer.setSize(window.innerWidth, window.innerHeight);
     containerRef.current.appendChild(renderer.domElement);
 
-    // for (let i = 0; i < 2; i++) {
-    //   const geometry = new THREE.BoxGeometry();
-    //   const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-    //   const cube = new THREE.Mesh(geometry, material);
-
-    //   cube.position.x = (Math.random() - 0.5) * 10;
-    //   cube.position.y = (Math.random() - 0.5) * 10;
-    //   cube.position.z = (Math.random() - 0.5) * 10;
-
-    //   scene.add(cube);
-    // }
-  
-    addImagePlane(scene, '/images/image2.jpg', new THREE.Vector3(0, 2, -3), 3, 'asd'); // 이미지의 너비를 10으로 지정
-    addImagePlane(scene, '/images/image2.jpg', new THREE.Vector3(3, 2, 0), 3, 'asd', new THREE.Vector3(0, -Math.PI / 2, 0)); // 이미지의 너비를 10으로 지정
-    addImagePlane(scene, '/images/image2.jpg', new THREE.Vector3(-3, 2, 0), 3, 'asd', new THREE.Vector3(0, Math.PI / 2, 0)); // 이미지의 너비를 10으로 지정
+    if(mapJson){
+      for(const mapDataNum in mapJson.defualt){
+        const frame = mapJson.defualt[mapDataNum];
+        addImagePlane(scene,
+          "/images/image2.jpg",
+          new THREE.Vector3(frame.coordinates[0], frame.coordinates[1], frame.coordinates[2]),
+          frame.width,
+          frame.name,
+          new THREE.Vector3(frame.rotation[0], frame.rotation[1], frame.rotation[2])
+          );       
+      }
+    }
 
     const geometry = new THREE.PlaneGeometry(40, 60); //(x,z) x-좌+우 z-앞+뒤
     const material = new THREE.MeshBasicMaterial({ color: 0xffffff, side: THREE.DoubleSide });
@@ -84,41 +89,106 @@ const Studio = () => {
     // Listen for window resize events
     window.addEventListener('resize', handleResize);
 
+    let movingCircle = 0;
+    let movingFlow = 0;
     const handleWheel = (event) => {//마우스 휠 event control 마우스휠
-      const delta = event.deltaY * 0.005; 
+      const delta = event.deltaY * 0.005;
+      
+      movingCircle += delta;
+      movingFlow +=delta;
 
-      //카메라 위치
-      // camera.position.z += delta;
+      if(movingCircle >= 0 && movingCircle < 3){//
+        camera.position.x = 0;
+        camera.position.z = 0;
+        camera.rotation.y = -movingCircle*3.1416/2/3;
+      }else if(movingCircle >= 3 && movingCircle < 6){
+        camera.position.x = 0;
+        camera.position.z = (movingCircle-3)*2;
+        camera.rotation.y = -1.5708;
+      }else if(movingCircle >= 6 && movingCircle < 9){
+        camera.position.x = 0;
+        camera.position.z = 6;
+        camera.rotation.y = -1.5708-(movingCircle-6)*1.5708/3;
+      }else if(movingCircle >= 9 && movingCircle < 15){
+        camera.position.x = -(movingCircle-9)*2;
+        camera.position.z = 6;
+        camera.rotation.y = -3.1416;
+      }else if(movingCircle >= 15 && movingCircle < 18){
+        camera.position.x = -12;
+        camera.position.z = 6;
+        camera.rotation.y = -3.1416-(movingCircle-3)*1.5708/3;
+      }else if(movingCircle >= 18 && movingCircle < 21){
+        camera.position.x = -12;
+        camera.position.z = 6-(movingCircle-18)*2;
+        camera.rotation.y = 1.5708;
+      }else if(movingCircle >= 21 && movingCircle < 27){
+        camera.position.x = -12;
+        camera.position.z = 0;
+        camera.rotation.y = 1.5708-(movingCircle-21)*1.5708/3;
+      }else if(movingCircle >= 27 && movingCircle < 28.5){
+        camera.position.x = -12+(movingCircle-27)*2;
+        camera.position.z = (movingCircle-27)*4;
+        camera.rotation.y = -1.5708+(movingCircle-3)*1.5708/3*2;
+      }else if(movingCircle >= 28.5 && movingCircle < 30){
+        camera.position.x = -9+(movingCircle-28.5)*2;
+        camera.position.z = 6-(movingCircle-28.5)*4;
+        camera.rotation.y = -1.5708+(movingCircle-3)*1.5708/3*2;
+      }else if(movingCircle >= 30 && movingCircle < 36){
+        camera.position.x = -6;
+        camera.position.z = 0;
+        camera.rotation.y = -(movingCircle-21)*1.5708/3;
+      }else if(movingCircle >= 36 && movingCircle < 37.5){
+        camera.position.x = -6+(movingCircle-36)*2;
+        camera.position.z = (movingCircle-36)*4;
+        camera.rotation.y = -1.5708+(movingCircle)*1.5708/3*2;
+      }else if(movingCircle >= 37.5 && movingCircle < 39){
+        camera.position.x = -3+(movingCircle-37.5)*2;
+        camera.position.z = 6-(movingCircle-37.5)*4;
+        camera.rotation.y = -1.5708+(movingCircle)*1.5708/3*2;
+      }else if(movingCircle >= 39 && movingCircle < 42){
+        camera.position.x = 0;
+        camera.position.z = 0;
+        camera.rotation.y = -1.5708-(movingCircle-21)*1.5708/3;
+      }else if(movingCircle < 0){
+        camera.position.x = 0;
+        camera.position.z = 0;
+        camera.rotation.y = 0;
+        movingCircle = 0;
+        if(movingFlow > 40){
+          movingFlow = movingFlow - movingFlow%42 + 42*(Math.round(Number(movingFlow%42))!==0)
+        }
+      }else if(movingCircle >= 42){
+        camera.position.x = 0;
+        camera.position.z = 0;
+        camera.rotation.y = 0;
+        movingCircle = 0;
+      }
 
-      //카메라 각도
-      const rotationSpeed = 0.1;
-      const rotationDelta = delta * rotationSpeed;
-
-      camera.rotation.y -= rotationDelta;
-
+      console.log(camera.position.x);
+      console.log(camera.position.z);
+      console.log(movingCircle);
+      console.log(movingFlow);
       renderer.render(scene, camera);
     };
 
     // Listen for wheel events for zooming
     containerRef.current.addEventListener('wheel', handleWheel);
 
-    // Animation
+    // Animation 효과 
     const animate = () => {
       requestAnimationFrame(animate);
 
-      // Rotate all cubes
-      // scene.children.forEach((child) => {
-      //   if (child instanceof THREE.Mesh) {
-      //     child.rotation.x += 0.01;
-      //     child.rotation.y += 0.01;
-      //   }
-      // });
+      // const frame1 = scene.getObjectByName('frame1');
+      // if (frame1 !== undefined) {
+      //   frame1.rotation.x += 0.01;
+      //   frame1.rotation.y += 0.01;
+      // }
 
       renderer.render(scene, cameraRef.current);
     };
 
     animate();
-
+    renderer.render(scene, cameraRef.current);
     const cleanup = () => {
       window.removeEventListener('resize', handleResize);
 
@@ -130,7 +200,7 @@ const Studio = () => {
     };
 
     return () => cleanup();
-  }, []);
+  }, [mapJson]);
 
   return <div ref={containerRef} style={{ width: '100%', height: '100vh', overflow: 'hidden' }} >
     <div className='sidebar-studio'></div>
