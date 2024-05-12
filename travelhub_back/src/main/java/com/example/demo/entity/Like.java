00a30fe9cc8ec23054b0c904568a1b3a7a2c262c
HANMINUOOK <dnrals7929@naver.com>
2024-05-12 16:22:09 +0900

    no frist haha

---
+++ travelhub_back/src/main/java/com/example/demo/entity/Like.java
@@ -0,0 +1,30 @@
+package com.example.demo.entity;
+import java.util.Date;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.TemporalType;
+import java.time.LocalDate;
+
+
+@Entity
+public class Like {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int like_id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user_id;
+
+    @ManyToOne
+    @JoinColumn(name = "travel_id", nullable = false)
+    private Travel travel_id;
+
+}
+
--- travelhub_back/src/main/java/com/example/demo/entity/User.java
+++ travelhub_back/src/main/java/com/example/demo/entity/User.java
@@ -1,18 +1,35 @@
 package com.example.demo.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import java.time.LocalDate;
 
 @Entity
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private int user_id;
 
-    private String username;
-    private String password;
-    private String email;
+    @Column(length = 50, nullable = false)
+    private String user_name;
+
+    @Column(length = 20, nullable = false)
+    private String user_password;
+
+    @Column(length = 11, nullable = false)
+    private String user_phoneNum;
+
+    @Column(nullable = false)
+    private LocalDate birthday;
+
+    @Column(length = 2)
+    private String sex;
+
+    @Column(nullable = false)
+    private LocalDate registrationDate;
+
 }
