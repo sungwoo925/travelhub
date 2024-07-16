a76df415ce274606d7f574df8b8b98492fa352fc
HANMINUOOK <dnrals7929@naver.com>
2024-07-16 16:20:47 +0900

    like Count up and down

--- travelhub_back/src/main/java/com/travelhub/repository/TravelRepository.java
+++ travelhub_back/src/main/java/com/travelhub/repository/TravelRepository.java
@@ -2,7 +2,8 @@ package com.travelhub.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.travelhub.entity.Travel;
-import java.util.List; 
+
+import java.util.List;
 
 public interface TravelRepository extends JpaRepository<Travel, Long> {
     List<Travel> findByHashtagContaining(String hashtag);
