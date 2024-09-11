ca3c837b00e9008df22f5bbb5425aab9c45cfb90
HANMINUOOK <dnrals7929@naver.com>
2024-09-11 20:17:02 +0900

    footer design

--- travelhub_front/src/components/Footer/Footer.js
+++ travelhub_front/src/components/Footer/Footer.js
@@ -1,31 +1,44 @@
-import React from 'react';
-import { useLocation } from 'react-router-dom'; 
-import './Footer.css';
+import React from "react";
+import { useLocation } from "react-router-dom";
+import "./Footer.css";
 
 const Footer = () => {
-  const location = useLocation(); 
+  const location = useLocation();
 
-  let className; 
-  if (location.pathname === '/login' || location.pathname === '/register') {
-    className = 'footer-login';
+  let className;
+  if (location.pathname === "/login" || location.pathname === "/register") {
+    className = "footer-login";
   } else {
-    className = 'footer-default';
+    className = "footer-default";
   }
-  
+
   return (
-    <footer className={className}>
-      <div className="footer-content">
-        <h4>Contact Us</h4>
-        <p>dnrals7929@naver.com</p>
-        <p>qkrtjddn5807@gmail.com</p>
-        <p>cks71714@naver.com</p>
+    <div className={`footer ${className}`}>
+      <div className="footer-inner">
+        <div className="footer-logo"></div>
+
+        <div className="info-area">
+          <ul className="info-list">
+            <li className="info-item">(주)웅진컴퍼스</li>
+            <li className="info-item">대표 : 서종윤</li>
+            <li className="info-item">사업자 등록번호 : 214-87-35792</li>
+            <li className="info-item">
+              통신판매신고 : 제 2005-서울서초-05631호
+            </li>
+          </ul>
+          <ul className="info-list">
+            <li className="info-item">
+              주소 : 서울시 서초구 강남대로 39길 15-10 한라비발디스튜디오 193,
+              3층
+            </li>
+            <li className="info-item">전화번호 : 02-3471-0013</li>
+          </ul>
+        </div>
+        <div className="copyright">
+          © woongjin compass. All Rights Reserved.
+        </div>
       </div>
-      <div className="footer-links">
-        <a href="/about">About Us</a>
-        <a href="/privacy">Privacy Policy</a>
-        <a href="/terms">Terms of Service</a>
-      </div>
-    </footer>
+    </div>
   );
 };
 
