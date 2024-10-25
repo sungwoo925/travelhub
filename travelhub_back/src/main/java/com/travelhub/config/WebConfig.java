5925a3b0f00cf030c868c90465376ff86e7bd2ae
kisuk <cks71714@naver.com>
2024-10-25 20:05:26 +0900

    home username loactaion add

--- travelhub_back/src/main/java/com/travelhub/config/WebConfig.java
+++ travelhub_back/src/main/java/com/travelhub/config/WebConfig.java
@@ -14,7 +14,9 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:8888")
+                        //.allowedOrigins("http://localhost:8888")
+
+                        .allowedOrigins("http://localhost:3000")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
